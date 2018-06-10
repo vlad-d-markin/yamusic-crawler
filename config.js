2abc15a92d6523a988fc2d70a9b67b97c5150f93
Vlad Markin <vlad.d.markin@gmail.com>
2018-06-10 10:23:38 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+    HEADLESS: false,
+    
+    viewport: {
+        WIDTH: 1200,
+        HEIGHT: 700
+    },
+    
+    urls: {
+        ROOT: '',
+        ALL_TRACKS: '',
+        LOGIN: 'https://passport.yandex.ru'
+    },
+    
+    selector: {
+        TRACK_ROW: 'div.page-users__tracks > div.longlist_tracks > div.longlist__cont > div.d-track',
+        TRACK_ROW_TITLE: 'div.d-track__name > a',
+        TRACK_ROW_ALBUM: '.d-track__artists > a',
+        FOOTER: 'div.footer',
+        ALL_TRACKS_BTN: 'div.centerblock-wrapper > div.centerblock > div > div > div.page-users__subhead > div.like-filter__right-side > nav > button.like-filter__all',
+        LOGIN_INPUT: '#root > div > div.passport-Page-Content > div.passport-Domik > input[name="login"]',
+        PASSWORD_INPUT: '#root > div > div.passport-Page-Content > div.passport-Domik > input[name="passwd"]',
+        LOGIN_SUBMIT: '#root > div > div.passport-Page-Content > div.passport-Domik > button.passport-Button[type="submit"]'
+    }
+};
---
+++ index.js
@@ -0,0 +1,139 @@
+const puppeteer = require('puppeteer');
+const Promise = require('bluebird');
+const _ = require('lodash');
+const crypto = require('crypto');
+
+const config = require('./config');
+
+
+
+function md5FromString(string) {
+    return crypto.createHash('md5').update(string).digest("hex");
+}
+
+
+function generateTrackId(track) {
+    return md5FromString(track.trackRef);
+}
+
+
+async function scrapeTracks(page) {
+    let tracks = {};
+    let tracksRaw = await page.evaluate(function() {
+        const extractedElements = document.querySelectorAll(config.selector.TRACK_ROW);
+        const items = [];
+        for (let element of extractedElements) {
+            let titleEl = element.querySelector(config.selector.TRACK_ROW_TITLE);
+            let artistEl = element.querySelector(config.selector.TRACK_ROW_ARTIST);
+            if (!titleEl || !artistEl) continue;
+            items.push({ 
+                title: titleEl.innerText, 
+                trackRef: titleEl.getAttribute('href'),
+                artist: artistEl.innerText,
+                artistRef: artistEl.getAttribute('href')
+            });
+        }
+        return items;
+    });
+
+    for (let track of tracksRaw) {
+        let trackId = generateTrackId(track);
+        tracks[trackId] = track;
+        tracks[trackId].id = trackId;
+    }
+    return Promise.resolve(tracks);
+}
+
+
+async function scrollPageTopToBottomAndScrape(page, scrape) {
+    let scrapeResults = {};
+    console.log('Scrolling page...');
+    await page.evaluate('window.scrollTo(0, 0)');
+    while (true) {        
+        let footerReached = await page.$eval(config.selector.FOOTER, function (elem){
+            var $elem = $(elem);
+            var $window = $(window);
+
+            var docViewTop = $window.scrollTop();
+            var docViewBottom = docViewTop + $window.height();
+
+            var elemTop = $elem.offset().top;
+            var elemBottom = elemTop + $elem.height();
+
+            return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        });
+        if (footerReached) break;
+        if (scrape) {
+            let result = await scrape(page);
+            _.merge(scrapeResults, result);
+        }
+        await page.evaluate('window.scrollBy(0, 80)');
+        await page.waitFor(80);
+    }
+    console.log('Scrolling page... Done!');
+    return Promise.resolve(scrapeResults);
+}
+
+
+
+async function passportLogin(page, login, password) {
+    await page.goto(config.urls.LOGIN);
+    await page.type(config.selector.LOGIN_INPUT, login);
+    await page.type(config.selector.PASSWORD_INPUT, password);
+    return Promise.all([
+        page.click(config.selector.LOGIN_SUBMIT),
+        await page.waitForNavigation()
+    ]);
+}
+
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: config.HEADLESS });
+    const page = await browser.newPage();
+  
+    await page.setViewport({ width: config.viewport.WIDTH, height: config.viewport.HEIGHT });
+    page.on('console', msg => {
+        console.log(`Browser >> ${msg.text()}`);
+    });
+  
+  //await page.goto('https://music.yandex.ru');
+  
+  //const loginBtn = await page.$('body > div.page-root.page-root_no-player > div.head > div.head__wrap > div.head__user > a');
+  /*
+  if(loginBtn) {
+      console.log('Not logged in');
+      await page.goto('https://passport.yandex.ru');
+      
+      await page.type(
+        '#root > div > div.passport-Page-Content > div.passport-Domik > div > div > div > div > div > form > div > label > input[name="login"]',
+        ''
+      );      
+      await page.type(
+        '#root > div > div.passport-Page-Content > div.passport-Domik > div > div > div > div > div > form > div > label > input[name="passwd"]',
+        ''
+      );
+      
+      await 
+
+      console.log('Login complete');
+  }
+  else {
+      console.log('Already logged in');
+  }
+  
+  */
+  
+
+    try {
+        await page.goto('https://music.yandex.ru/users/vlad.d.markin/tracks');
+        await page.click(config.selector.ALL_TRACKS_BTN);
+        await page.waitFor(2000);
+
+    let tracks = await scrollPageTopToBottomAndScrape(page, scrapeTracks);
+    //console.log(tracks);
+  }
+  catch (e) {
+      console.error(e);
+  }
+})();
+
